296bafda19a1c7e84b64d8d7477ed82a5baf31e5
jasminescott18 <jasminescott2018@gmail.com>
2022-01-28 00:02:46 -0500

    Added: Connected to Firebase, worked on Pets List

---
+++ app/src/main/java/edu/rosehulman/leash/adapters/PetsAdapter.kt
@@ -0,0 +1,103 @@
+package edu.rosehulman.leash.adapters
+
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.util.rangeTo
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import edu.rosehulman.leash.R
+import edu.rosehulman.leash.models.Pet
+import edu.rosehulman.leash.models.PetsViewModel
+import edu.rosehulman.leash.ui.PetsFragment
+
+class PetsAdapter(val fragment: PetsFragment) : RecyclerView.Adapter<PetsAdapter.PetsViewHolder>() {
+
+    val model = ViewModelProvider(fragment.requireActivity()).get(PetsViewModel::class.java)
+
+    /** This gets fired every time an adapter is made
+     * Just added to make sure that connection between app
+     * and Firebase works **/
+    init {
+          // Add data to a Firestore collection
+//          Firebase.firestore.collection("Pets").add(Pet("Rudolph", Timestamp.now(), "dog"))
+    }
+
+    fun addListener(fragmentName: String) {
+        model.addListener(fragmentName) {
+            notifyDataSetChanged()
+        }
+    }
+
+    fun removeListener(fragmentName: String) {
+        model.removeListener(fragmentName)
+    }
+
+    /** Called when RecyclerView needs a new [ViewHolder] of the given type to represent...*/
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.pet_item, parent, false)
+        return PetsViewHolder(view)
+    }
+
+    /** Called by RecyclerView to display the data at the specified position. This method should...*/
+    override fun onBindViewHolder(holder: PetsViewHolder, position: Int) {
+        holder.bind(model.getPetAt(position))
+    }
+
+    /** Returns the total number of items in the data set held by the adapter...*/
+    override fun getItemCount() = model.size()
+
+
+    /*
+    This is where we add a photo; Calls the add method located in the PhotosViewModel
+     */
+    fun addPet() {
+        model.addPet()
+        notifyDataSetChanged()
+    }
+
+    inner class PetsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val petNameTextView: TextView = itemView.findViewById(R.id.pet_name_textView)
+        val petTypeTextView: TextView = itemView.findViewById(R.id.pet_type_textView)
+        val petBirthdateTextView: TextView = itemView.findViewById(R.id.pet_birthdate_textView)
+
+        init {
+//            itemView.setOnClickListener {
+//                model.updatePos(adapterPosition)
+//                fragment.findNavController().navigate(R.id.nav_edit_photos,
+//                    null,
+//                    // Simple animation when sliding between pages
+//                    navOptions {
+//                        anim {
+//                            enter = R.anim.slide_in_left
+//                            exit = R.anim.slide_out_right
+//                        }
+//                    })
+        }
+
+//            itemView.setOnLongClickListener{
+//                model.updatePos(adapterPosition)
+//                model.toggleCurrentPhoto()
+//                model.updateCurrentPhoto(model.getCurrentPhoto().url, model.getCurrentPhoto().caption, model.getCurrentPhoto().isSelected)
+////                notifyDataSetChanged()
+//                true
+//            }
+
+        // This method needs to be fast! It can get called many many many times over!!!
+        fun bind(pet: Pet) {
+            petNameTextView.text = pet.name
+            petTypeTextView.text = "Pet Type: ${pet.type}"
+            petBirthdateTextView.text = "Birthdate: ${parseDate(pet.birthdate)}"
+        }
+    }
+
+    fun parseDate(time: Timestamp): String {
+        return "${time.toDate().month.toString() + 1}/${time.toDate().date.toString()}/${time.toDate().year.toString().substring(1,3)}"
+    }
+}
--- app/src/main/java/edu/rosehulman/leash/ui/PetsFragment.kt
+++ app/src/main/java/edu/rosehulman/leash/ui/PetsFragment.kt
@@ -9,31 +9,53 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.rosehulman.leash.R
+import edu.rosehulman.leash.adapters.PetsAdapter
 import edu.rosehulman.leash.databinding.FragmentPetsBinding
 import edu.rosehulman.leash.models.PetsViewModel
 
 class PetsFragment : Fragment() {
 
+    // The file that will displaying the list; normal binding conventions here
     private lateinit var binding: FragmentPetsBinding
+    private lateinit var adapter: PetsAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentPetsBinding.inflate(inflater, container, false)
+        // Make Recycler; Connect with Adapter
+        adapter = PetsAdapter(this)
+        // Set Recyclerview and Adapter properties below
+        binding.recyclerView.adapter = adapter
+        adapter.addListener(fragmentName)
+        // Make the items show up in a grid, with 3 items in each row
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
 
-       setupButtons()
+        binding.fab.setOnClickListener {
+            adapter.addPet()
+        }
 
         return binding.root
     }
 
-
-    fun setupButtons() {
-        binding.fab.setOnClickListener {
-            findNavController().navigate(R.id.navigation_pets_edit)
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        adapter.removeListener(fragmentName)
     }
 
+    companion object {
+        const val fragmentName = "PetsFragment"
+    }
 }
