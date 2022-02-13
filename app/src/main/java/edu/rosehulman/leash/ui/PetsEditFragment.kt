591b0a54f163236e81e52c3ae53db50ceab311ab
jasminescott18 <jasminescott2018@gmail.com>
2022-02-13 15:50:35 -0500

    Added: Updating Pets fully works

--- app/src/main/java/edu/rosehulman/leash/ui/PetsEditFragment.kt
+++ app/src/main/java/edu/rosehulman/leash/ui/PetsEditFragment.kt
@@ -7,11 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointBackward
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.firebase.Timestamp
 import edu.rosehulman.leash.R
 import edu.rosehulman.leash.databinding.FragmentPetsCreateBinding
 import edu.rosehulman.leash.databinding.FragmentPetsEditBinding
 import edu.rosehulman.leash.models.PetsViewModel
+import java.util.*
 
 class PetsEditFragment : Fragment() {
 
@@ -19,6 +23,8 @@ class PetsEditFragment : Fragment() {
     private lateinit var model: PetsViewModel
 
     private lateinit var binding: FragmentPetsEditBinding
+
+    private lateinit var timestamp: Date
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,14 +42,35 @@ class PetsEditFragment : Fragment() {
 
     fun updateView() {
         binding.nameEditEditText.setText(model.getCurrentPet().name)
-        binding.birthdateEditEditText.setText(model.getCurrentPet().birthdate.toString())
+        binding.birthdateEditEditText.setText(model.getCurrentPet().birthdate.toDate().toString())
         binding.petTypeEditEditText.setText(model.getCurrentPet().type)
     }
 
     // TODO: Fix time parameter, for now uses timestamp.now()...
     fun setupButtons() {
+        // Logic for choosing a birthdate
+        binding.birthdateEditEditText.setOnClickListener {
+            val constraintsBuilder =
+                CalendarConstraints.Builder()
+                    .setValidator(DateValidatorPointBackward.now())
+
+            val datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Select date")
+                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                    .setInputMode(MaterialDatePicker.INPUT_MODE_TEXT)
+                    .setCalendarConstraints(constraintsBuilder.build())
+                    .build()
+            datePicker.addOnPositiveButtonClickListener {
+
+                timestamp = datePicker.selection?.let { it1 -> Date(it1) }!!
+                binding.birthdateEditEditText.setText(datePicker.headerText)
+            }
+            datePicker.show(parentFragmentManager, "tag");
+        }
+
         binding.savePetEditButton.setOnClickListener{
-            model.updateCurrentPet(binding.nameEditEditText.text.toString(), Timestamp.now(),
+            model.updateCurrentPet(binding.nameEditEditText.text.toString(), Timestamp(timestamp),
                     binding.petTypeEditEditText.text.toString())
             this.findNavController().navigate(R.id.navigation_pets)
         }
