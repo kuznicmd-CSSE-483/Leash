74435d941063350f311b7b6609241f2b579460b7
jasminescott18 <jasminescott2018@gmail.com>
2022-02-13 20:21:11 -0500

    Added: Navigation to Pet's Detail Page

--- app/src/main/java/edu/rosehulman/leash/ui/PetsDetailFragment.kt
+++ app/src/main/java/edu/rosehulman/leash/ui/PetsDetailFragment.kt
@@ -8,13 +8,20 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import edu.rosehulman.leash.R
 import edu.rosehulman.leash.databinding.FragmentPetsBinding
 import edu.rosehulman.leash.databinding.FragmentPetsDetailBinding
+import edu.rosehulman.leash.models.EventsViewModel
 import edu.rosehulman.leash.models.PetsViewModel
 
 class PetsDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentPetsDetailBinding
+    private lateinit var model: PetsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +29,19 @@ class PetsDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentPetsDetailBinding.inflate(inflater, container, false)
+        model = ViewModelProvider(requireActivity()).get(PetsViewModel::class.java)
+
+        updateView()
         return binding.root
+    }
+
+    fun updateView() {
+        binding.petNameTextView.text = model.getCurrentPet().name
+        binding.birthdateTextView.text = "Birthdate: ${parseDate(model.getCurrentPet().birthdate)}"
+    }
+
+    fun parseDate(time: Timestamp): String {
+        return "${time.toDate().month + 1}/${time.toDate().date.toString()}/${time.toDate().year.toString()}"
     }
 
 }
